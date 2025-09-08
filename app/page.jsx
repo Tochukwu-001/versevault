4bbd8ec25d773a9fe39be76532c1d3f9310fb58c
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-09-08 12:34:48 +0100

    updates

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-dvh">
       <h1>Hello World</h1>
     </main>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,14 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { FaRegUser } from "react-icons/fa6";
 import { RiMenu3Line } from "react-icons/ri";
+import { IoCloseOutline } from "react-icons/io5";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   const navItems = [
     { label: "Home", url: "/" },
     { label: "About Us", url: "/about" },
@@ -14,8 +17,8 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="shadow-md px-8 py-3 flex items-center justify-between">
-      <Link href={"/"} className="flex items-center gap-1">
+    <nav className="shadow-md md:px-8 px-2 py-3 flex items-center justify-between">
+      <Link href={"/"} className="flex items-center gap-1 z-40">
         <Image
           src={"/logo.png"}
           alt="logo"
@@ -39,16 +42,36 @@ const Navbar = () => {
         ))}
       </div>
 
+      {/* mobile and tab view */}
+      <div
+        className={`lg:hidden bg-white h-dvh w-full top-0 left-0 absolute flex flex-col items-center gap-10 pt-20 ${
+          navOpen ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        {navItems.map((item, index) => (
+          <Link
+            key={index}
+            href={item.url}
+            className="text-gray-800 hover:text-purple-600 transition-all duration-150 text-lg"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
+
       <Link
         href={"#"}
-        className="flex items-center gap-1 text-lg hover:text-purple-600 transition-all duration-150"
+        className="flex items-center gap-1 text-lg hover:text-purple-600 transition-all duration-150 max-lg:ml-auto z-40"
       >
-        <p className="max-md:hidden">Sign In</p>        
+        <p className="max-md:hidden">Sign In</p>
         <FaRegUser />
       </Link>
 
-      <button>
-        <RiMenu3Line/>
+      <button
+        onClick={() => setNavOpen(!navOpen)}
+        className="lg:hidden text-2xl ml-2 z-40"
+      >
+        {navOpen ? <IoCloseOutline /> : <RiMenu3Line />}
       </button>
     </nav>
   );
