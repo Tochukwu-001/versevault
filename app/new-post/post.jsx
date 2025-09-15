94355d5a8bddbce606f75e402613deef974793ce
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-09-15 12:33:43 +0100

    updates

--- app/new-post/post.jsx
+++ app/new-post/post.jsx
@@ -1,12 +1,22 @@
 "use client";
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 
 const NewPostForm = () => {
-  return (
-    <main className='min-h-dvh'>
-      New form
-    </main>
-  )
+    return (
+        <main className='min-h-dvh'>
+            <div>
+                <h1>Share your native stories and poems with a larger community</h1>
+
+                <div>
+                    <Formik>
+                        
+                    </Formik>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default NewPostForm
